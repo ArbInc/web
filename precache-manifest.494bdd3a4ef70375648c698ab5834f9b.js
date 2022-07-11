033ba0ce1e27dac8bee7db1011407b331d43fce6
d3vnull10 <D3vnull10@gmail.com>
2022-07-11 16:19:46 +0200

    Updates

---
+++ precache-manifest.494bdd3a4ef70375648c698ab5834f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0855a9bd1cc3f4d8ae1a0bc429772aaa",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "a806d7c97163b309e104",
+    "url": "/web/static/css/2.3342267d.chunk.css"
+  },
+  {
+    "revision": "e87bcdeb088f7aafad2c",
+    "url": "/web/static/css/main.e7b7afcf.chunk.css"
+  },
+  {
+    "revision": "a806d7c97163b309e104",
+    "url": "/web/static/js/2.462487b5.chunk.js"
+  },
+  {
+    "revision": "e87bcdeb088f7aafad2c",
+    "url": "/web/static/js/main.c1f2602d.chunk.js"
+  },
+  {
+    "revision": "65e0b2fae3337b7c9f53",
+    "url": "/web/static/js/runtime-main.dd8c1b7f.js"
+  }
+]);
